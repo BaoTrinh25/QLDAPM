ab8c28ea50b980ad7d81bdc7a57dc03e7ef3af4a
BaoTrinh25 <btrinh2505@gmail.com>
2024-08-04 13:14:14 +0700

    merge Front-end from branch baotrinhh

---
+++ Front-end/jobapp-reactjs/src/configs/Reducers.js
@@ -0,0 +1,21 @@
+const MyUserReducer = (current, action) => {
+    switch (action.type) {
+        case "login":
+            return action.payload;
+        // case 'update_applicant':
+        //     return {
+        //         ...current,
+        //         applicant: action.payload,
+        //     };
+        // case 'update_employer':
+        //     return {
+        //         ...current,
+        //         employer: action.payload,
+        //     };
+        case "logout":
+            return null;
+    }
+    return current;
+}
+
+export default MyUserReducer;
